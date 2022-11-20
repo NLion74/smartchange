36d2b072284f47ecf91ff1c7227ac183dfeae9fe
NLion74 <97677226+NLion74@users.noreply.github.com>
2022-11-20 21:29:33 +0100

    Added change detection

--- smartchange.sh
+++ smartchange.sh
@@ -10,7 +10,16 @@ if [ ! -d "logs" ]; then
     mkdir logs
 fi
 
+if [ ! -d "changes" ]; then
+    mkdir changes
+fi
+
 date=$(date)
+unset -v latest
+for file in "logs"/*; do
+  [[ $file -nt $latest ]] && latest=$file
+done
+
 echo "-----------------------------------------------------" >> "logs/smartvalue - ${date}.txt"
 echo "${date}" >> "logs/smartvalue - ${date}.txt"
 
@@ -21,12 +30,25 @@ if [[ ! -e $drive ]]; then continue ; fi
 
 echo -n "$drive "
 
-rsc=$(smartctl -a $drive 2>/dev/null | grep Reallocated_Sector_Ct | awk '{ print $10 }')
-ru=$(smartctl -a $drive 2>/dev/null | grep Reported_Uncorrect | awk '{ print $10 }')
-ct=$(smartctl -a $drive 2>/dev/null | grep Command_Timeout | awk '{ print $10 " " $11 " " $12 }')
-cps=$(smartctl -a $drive 2>/dev/null | grep Current_Pending_Sector | awk '{ print $10 }')
-ou=$(smartctl -a $drive 2>/dev/null | grep Offline_Uncorrectable | awk '{ print $10 }')
-smarthealth=$(smartctl -H $drive 2>/dev/null | grep '^SMART overall' | awk '{ print $6 }')
+changersc=$(grep -A 8 "$drive" "$file" 2>/dev/null | grep Reallocated_Sector_Ct |  cut -d "=" -f2)
+changeru=$(grep -A 8 "$drive" "$file" 2>/dev/null | grep Reported_Uncorrect |  cut -d "=" -f2)
+changect=$(grep -A 8 "$drive" "$file" 2>/dev/null | grep Command_Timeout |  cut -d "=" -f2)
+changecps=$(grep -A 8 "$drive" "$file" 2>/dev/null | grep Current_Pending_Sector |  cut -d "=" -f2)
+changeou=$(grep -A 8 "$drive" "$file" 2>/dev/null | grep Offline_Uncorrectable |  cut -d "=" -f2)
+rsc=$(smartctl -a "$drive" 2>/dev/null | grep Reallocated_Sector_Ct | awk '{ print $10 }')
+ru=$(smartctl -a "$drive" 2>/dev/null | grep Reported_Uncorrect | awk '{ print $10 }')
+ct=$(smartctl -a "$drive" 2>/dev/null | grep Command_Timeout | awk '{ print $10 " " $11 " " $12 }')
+cps=$(smartctl -a "$drive" 2>/dev/null | grep Current_Pending_Sector | awk '{ print $10 }')
+ou=$(smartctl -a "$drive" 2>/dev/null | grep Offline_Uncorrectable | awk '{ print $10 }')
+smarthealth=$(smartctl -H "$drive" 2>/dev/null | grep '^SMART overall' | awk '{ print $6 }')
+
+# change detection
+
+if [[ $rsc == '' || $changersc == '' ]]; then true; elif [ "$rsc" != "$changersc" ]; then echo "$date | $drive | Reallocated_Sector_Ct=$rsc --> $changersc" >> "changes/smartchangelog - ${date}.txt"; fi
+if [[ $ru == '' || $changeru == '' ]]; then true; elif [ "$ru" != "$changeru" ]; then echo "$date | $drive | Reallocated_Sector_Ct=$ru --> $changeru" >> "changes/smartchangelog - ${date}.txt"; fi
+if [[ $ct == '' || $changect == '' ]]; then true; elif [ "$ct" != "$changect" ]; then echo "$date | $drive | Reallocated_Sector_Ct=$ct --> $changect" >> "changes/smartchangelog - ${date}.txt"; fi
+if [[ $cps == '' || $changecps == '' ]]; then true; elif [ "$cps" != "$changecps" ]; then echo "$date | $drive | Reallocated_Sector_Ct=$cps --> $changecps" >> "changes/smartchangelog - ${date}.txt"; fi
+if [[ $ou == '' || $changeou == '' ]]; then true; elif [ "$ou" != "$changeou" ]; then echo "$date | $drive | Reallocated_Sector_Ct=$ou --> $changeou" >> "changes/smartchangelog - ${date}.txt"; fi
 
 # smartvalue log
 

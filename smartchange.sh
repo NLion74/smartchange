cea9a24e6977bc97a51d854115a4e8401a2be6c4
NLion74 <97677226+NLion74@users.noreply.github.com>
2022-11-21 14:20:43 +0100

    Added Comments

--- smartchange.sh
+++ smartchange.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Detecting if smartctl is installed
+
 if ! command -v smartctl >/dev/null ; then
 	echo "This script requires smartctl to be installed"
 	echo ""
 	apt install smartctl
 fi
+
+# Looking for existing logs and changes directory
 
 if [ ! -d "logs" ]; then
     mkdir logs
@@ -14,14 +18,19 @@ if [ ! -d "changes" ]; then
     mkdir changes
 fi
 
-date=$(date)
+# Getting latest Smartvaluelog
 unset -v latest
 for file in "logs"/*; do
   [[ $file -nt $latest ]] && latest=$file
 done
 
+# Initiating Smartvaluelog
+
+date=$(date)
 echo "-----------------------------------------------------" >> "logs/smartvalue - ${date}.txt"
 echo "${date}" >> "logs/smartvalue - ${date}.txt"
+
+# Detecting Drives
 
 for drive in /dev/sd[a-z] /dev/sd[a-z][a-z]
 do
@@ -29,6 +38,8 @@ do
 if [[ ! -e $drive ]]; then continue ; fi
 
 echo -n "$drive "
+
+# detecting changes, collecting smartctl data and running smarthealth test 
 
 changersc=$(grep -A 8 "$drive" "$file" 2>/dev/null | grep Reallocated_Sector_Ct |  cut -d "=" -f2)
 changeru=$(grep -A 8 "$drive" "$file" 2>/dev/null | grep Reported_Uncorrect |  cut -d "=" -f2)
@@ -42,7 +53,7 @@ cps=$(smartctl -a "$drive" 2>/dev/null | grep Current_Pending_Sector | awk '{ pr
 ou=$(smartctl -a "$drive" 2>/dev/null | grep Offline_Uncorrectable | awk '{ print $10 }')
 smarthealth=$(smartctl -H "$drive" 2>/dev/null | grep '^SMART overall' | awk '{ print $6 }')
 
-# change detection
+# Creating Changelog
 
 if [[ $rsc == '' || $changersc == '' ]]; then true; elif [ "$rsc" != "$changersc" ]; then echo "$date | $drive | Reallocated_Sector_Ct=$rsc --> $changersc" >> "changes/smartchangelog - ${date}.txt"; fi
 if [[ $ru == '' || $changeru == '' ]]; then true; elif [ "$ru" != "$changeru" ]; then echo "$date | $drive | Reallocated_Sector_Ct=$ru --> $changeru" >> "changes/smartchangelog - ${date}.txt"; fi
@@ -50,7 +61,7 @@ if [[ $ct == '' || $changect == '' ]]; then true; elif [ "$ct" != "$changect" ];
 if [[ $cps == '' || $changecps == '' ]]; then true; elif [ "$cps" != "$changecps" ]; then echo "$date | $drive | Reallocated_Sector_Ct=$cps --> $changecps" >> "changes/smartchangelog - ${date}.txt"; fi
 if [[ $ou == '' || $changeou == '' ]]; then true; elif [ "$ou" != "$changeou" ]; then echo "$date | $drive | Reallocated_Sector_Ct=$ou --> $changeou" >> "changes/smartchangelog - ${date}.txt"; fi
 
-# smartvalue log
+# Creating Smartvaluelog
 
 echo "" >> "logs/smartvalue - ${date}.txt"
 echo "drive=${drive}" >> "logs/smartvalue - ${date}.txt"
@@ -63,6 +74,8 @@ echo "Command_Timeout(188)=${ct}" >> "logs/smartvalue - ${date}.txt"
 echo "Current_Pending_Sector(197)=${cps}" >> "logs/smartvalue - ${date}.txt"
 echo "Offline_Uncorrectable(198)=${ou}" >> "logs/smartvalue - ${date}.txt"
 echo "" >> "logs/smartvalue - ${date}.txt"
+
+# Printing Smarthealthcheck Result
 
 echo "$smarthealth"
 
